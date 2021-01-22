aace47f6873ebedd09fbac6732457427c68ad3a2
StefanLotzmann <stefan.lotzmann@gmail.com>
2021-01-22 11:50:35 +0100

    Version 1, Update 1: some corrected comments

--- Netzplan_PERT_dreieckverteilte VD.R
+++ Netzplan_PERT_dreieckverteilte VD.R
@@ -7,42 +7,41 @@ opt = c( 93,110, 88,0,19,30,0,12,26, 8,20,5,2)
 mol = c(108,135,116,0,23,35,0,15,33,10,24,6,5)
 pes = c(195,190,120,0,33,46,0,24,46,12,34,7,8)
 
-#### Funktion zur Berechnung von Mittelwert,Varianz und Standardabweichung für Modell mit Normalverteilung ####
-est =  function(opt,act,pes)
-{
-  mu  = (opt+mol+pes)/3
-  var = ((opt-pes)^2+(pes-mol)^2+(opt-mol)^2)/36
-  sd  = sqrt(var)
-  mat = round(data.frame(mu,sd,var),2)
-  print(mat)
+#### Funktion zur Berechnung von Mittelwert,Varianz und Standardabweichung für Modell mit Dreieckverteilung ####
+est =  function(opt,act,pes) {
+    mu  = (opt+mol+pes)/3
+    var = ((opt-pes)^2+(pes-mol)^2+(opt-mol)^2)/36
+    sd  = sqrt(var)
+    mat = round(data.frame(mu,sd,var),2)
+    print(mat)
 }
 n = est(opt,mol,pes)
 
 #### Definition der Präzedenz-Matrix aller Vorgänge ####
-prec1and2<-matrix(c(0,1,1,0,0,0,0,0,0,0,0,0,0,
-                    0,0,0,1,1,0,0,0,0,0,0,0,0,
-                    0,0,0,1,0,0,0,0,0,0,0,0,0,
-                    0,0,0,0,0,1,0,0,0,0,0,0,0,
-                    0,0,0,0,0,0,1,0,1,0,0,0,0,
-                    0,0,0,0,0,0,0,1,0,0,0,0,0,
-                    0,0,0,0,0,0,0,1,0,0,0,0,0,
-                    0,0,0,0,0,0,0,0,0,1,0,0,0,
-                    0,0,0,0,0,0,0,0,0,0,1,0,0,
-                    0,0,0,0,0,0,0,0,0,0,1,0,0,
-                    0,0,0,0,0,0,0,0,0,0,0,1,0,
-                    0,0,0,0,0,0,0,0,0,0,0,0,1,
-                    0,0,0,0,0,0,0,0,0,0,0,0,0),nrow=13,ncol=13,byrow=TRUE) 
+prec1and2 <- matrix(c(0,1,1,0,0,0,0,0,0,0,0,0,0,
+                      0,0,0,1,1,0,0,0,0,0,0,0,0,
+                      0,0,0,1,0,0,0,0,0,0,0,0,0,
+                      0,0,0,0,0,1,0,0,0,0,0,0,0,
+                      0,0,0,0,0,0,1,0,1,0,0,0,0,
+                      0,0,0,0,0,0,0,1,0,0,0,0,0,
+                      0,0,0,0,0,0,0,1,0,0,0,0,0,
+                      0,0,0,0,0,0,0,0,0,1,0,0,0,
+                      0,0,0,0,0,0,0,0,0,0,1,0,0,
+                      0,0,0,0,0,0,0,0,0,0,1,0,0,
+                      0,0,0,0,0,0,0,0,0,0,0,1,0,
+                      0,0,0,0,0,0,0,0,0,0,0,0,1,
+                      0,0,0,0,0,0,0,0,0,0,0,0,0),nrow=13,ncol=13,byrow=TRUE) 
 
 #### Berechnung des PERT-Netzplanes (Vor- und Rückwärtsterminierung) ####
 schedule.pert(prec1and2=prec1and2, duration = n$mu, PRINT=TRUE)
 
-#### Definition der PDF f??r jeden Vorgang ####
-distribution<-c("TRIANGLE","TRIANGLE","TRIANGLE","TRIANGLE","TRIANGLE","TRIANGLE","TRIANGLE","TRIANGLE","TRIANGLE","TRIANGLE","TRIANGLE","TRIANGLE","TRIANGLE")
+#### Definition der PDF für jeden Vorgang ####
+distribution <- c("TRIANGLE","TRIANGLE","TRIANGLE","TRIANGLE","TRIANGLE","TRIANGLE","TRIANGLE","TRIANGLE","TRIANGLE","TRIANGLE","TRIANGLE","TRIANGLE","TRIANGLE")
 
-#### Erzeugen einer Matrix mit den notwendigen Verteilungsparametern (Normalverteilung) ####
+#### Erzeugen einer Matrix mit den notwendigen Verteilungsparametern (Dreieckverteilung) ####
 values = matrix(c(opt,pes,mol),13,3 );values
 
-#### Simulation des PERT-Netzplanes unter der Annahme Vorgansdauern sind i.i.d. N(mu,var) ####
+#### Simulation des PERT-Netzplanes unter der Annahme Vorgansdauern sind i.i.d. Tri(a,c,b) ####
 set.seed(1024)
 stochastic.pert(prec1and2=prec1and2,distribution=distribution,values=values, percentile = 0.95, compilations = 100000)
 
